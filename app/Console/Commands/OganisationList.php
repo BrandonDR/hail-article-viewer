c155ebda02c145242e68e718d017a3dac1dd44ff
Brandon De Rose <brandon@entuitive.co.nz>
2022-01-21 22:10:12 +1300

    Added command description

--- app/Console/Commands/OganisationList.php
+++ app/Console/Commands/OganisationList.php
@@ -19,7 +19,7 @@ class OganisationList extends Command
      *
      * @var string
      */
-    protected $description = 'Command description';
+    protected $description = 'List organisations of the authenticated user';
 
     /**
      * Create a new command instance.
