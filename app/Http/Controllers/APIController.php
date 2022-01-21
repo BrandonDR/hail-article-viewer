6c03159548422f7e6c94e7e7cb717d2375053efd
Brandon De Rose <brandon@entuitive.co.nz>
2022-01-21 21:47:35 +1300

    Clean up

--- app/Http/Controllers/APIController.php
+++ app/Http/Controllers/APIController.php
@@ -10,13 +10,11 @@ class APIController extends Controller
     public function getArticles(HailService $hail)
     {
         if (Cache::has('hail_articles')) {
-            info('using cache');
             return response()->json(Cache::get('hail_articles'));
         }
 
         $articles = HailService::getArticles();
 
-        info('NOT using cache');
         Cache::put('hail_articles', $articles, now()->addMinutes(5));
 
         return response()->json($articles);
