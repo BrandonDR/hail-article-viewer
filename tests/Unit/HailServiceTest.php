f36c00063a0a4808f0d82611f92395cd6319c326
Brandon De Rose <brandon@entuitive.co.nz>
2022-01-21 21:47:27 +1300

    Added organisations test case

--- tests/Unit/HailServiceTest.php
+++ tests/Unit/HailServiceTest.php
@@ -13,6 +13,18 @@ class HailServiceTest extends TestCase
 
         $this->assertIsArray($articles);
         $this->assertGreaterThan(0, count($articles));
+        $this->assertObjectHasAttribute('id', $articles[0]);
         $this->assertObjectHasAttribute('title', $articles[0]);
+        $this->assertObjectHasAttribute('url', $articles[0]);
+    }
+
+    public function test_retrieve_organisations()
+    {
+        $organisations = HailService::getOrganisations();
+
+        $this->assertIsArray($organisations);
+        $this->assertGreaterThan(0, count($organisations));
+        $this->assertObjectHasAttribute('id', $organisations[0]);
+        $this->assertObjectHasAttribute('name', $organisations[0]);
     }
 }
