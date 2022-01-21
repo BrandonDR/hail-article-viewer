971dd9d171942253eadfef19710dfeb1da53aa29
Brandon De Rose <brandon@entuitive.co.nz>
2022-01-21 21:28:36 +1300

    Prevent the user from opening unpublished articles

--- public/js/articles.js
+++ public/js/articles.js
@@ -13,6 +13,16 @@ const ArticleViewer = {
             return this.articles.length == 0;
         }
     },
+    methods: {
+        openArticle(article) {
+            if (article.status != 'published') {
+                alert('Article is not published');
+                return;
+            }
+
+            window.open(article.url, '_blank');
+        }
+    },
     mounted() {
         axios.get('/api/articles').then(response => {
             this.articles = response.data;
